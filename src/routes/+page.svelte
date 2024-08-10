ce0b6e8ee4316007c0bd6f13b64f870c44f35281
Artur <arthur.sogomonyan@gmail.com>
2024-08-10 20:13:13 +0200

    added some descriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,13 @@
 	import LandingCanvas from '../components/LandingCanvas.svelte';
 
 	import { Tabs, TabItem } from 'flowbite-svelte';
-	import { Breadcrumb, BreadcrumbItem, Card, Button, Toggle } from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Card, Button, Toggle, Badge } from 'flowbite-svelte';
 	import { ArrowRightOutline } from 'flowbite-svelte-icons';
 
+	import { base } from '$app/paths';
+
 	import rail4future from '$lib/images/rail4future.jpg';
+	import susteam from '$lib/images/susteam.png';
 
 	function scrollDown() {
 		window.scrollTo({
@@ -60,18 +63,34 @@
 <Tabs class='menu'>
 	<!-- <a href="#"> -->
 	<TabItem class='menu-tab' open title="Projects" on:click={scrollDown}>
-	  	<section class="projects">
+		<section class="projects">
 			<Card img={rail4future} class="project">
 				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">Rail4Future Data Science Project 2024</h5>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+					Analyzing rail irregularities for industrial partners Siemens and ÖBB.
+				</p>
+				<p>
+					<Badge border rounded color="yellow">LSTM</Badge>
+					<Badge border rounded color="red">Tensorflow</Badge>
+					<Badge border rounded color="green">Data Science</Badge>
+				</p>
+				<br>
+		
 				<Button class="bg-primary-600" href="https://www.rail4future.com/en/" target="_blank">
 				Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
 				</Button>
 			</Card>
-			<Card img={rail4future} class="project">
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-				<Button class="bg-primary-600">
+			<Card img={susteam} class="project">
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-800 dark:text-white">susteam</h5>
+				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Full-stack development of ESG platform. Generation of the Reports for hospitality industry.</p>
+				
+				<p>
+					<Badge border rounded color="yellow">React.js</Badge>
+					<Badge border rounded color="blue">FastAPI</Badge>
+					<Badge border rounded color="red">Bert, LLama3</Badge>
+				</p>
+				<br>
+				<Button class="bg-primary-600" href="https://susteam.at/" target="_blank">
 				Read more <ArrowRightOutline class="w-6 h-6 ms-2 text-white" />
 				</Button>
 			</Card>
@@ -93,13 +112,16 @@
 	</TabItem>
 	<!-- </a> -->
 	<TabItem class='menu-tab' title="Education" on:click={scrollDown}>
-	  <p>
-		<b>Education</b>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-	  </p>
+		<section class="expirience">
+			<div class="education">
+				<p><b>Data Science — Master of Science</b> <br> Technische Universität Wien, Austria, 2022 - now</p>
+				<p><b>Information Technologies and Telecommunications — Bachelor of Science</b> <br> Bauman Moscow State Technical University, 2014 - 2020</p>
+				<p><b>Deep Learning — Deep Learning School</b> <br> Moscow Institute of Physics and Technology, 2020 - 2021</p>
+				<p><b>Machine Learning — School of Data Analysis</b> <br> Higher School of Economics University and Yandex, 2020</p>
+			</div>
+		</section>
 	</TabItem>
 	<TabItem class='menu-tab' title="Work Expirience" on:click={scrollDown}>
-		<b>Work Expirience</b>
 		<div class="work-experience">
 			<p><b>Data Scientist, Project Rail4Future</b> <br> Technische Universität Wien, Feb 2024 - Jul 2024</p>
 			<ul>
@@ -123,70 +145,76 @@
 	  <!-- </p> -->
 	</TabItem>
 	<TabItem class='menu-tab' title="Skills" on:click={scrollDown}>
-	  <p>
-		<b>Dashboard:</b>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-	  </p>
+		<section>
+			<div class="skills">
+				<p><b>Software Development:</b> Docker, Python, C++, R, Django, Flask, FastAPI, JavaScript, React, Svelte</p>
+				<p><b>Machine Learning:</b> scikit-learn, Pandas, NumPy, Tensorflow, PyTorch, OpenCV, YOLO, NLTK, SpaCy, Bert, LLama3, GPT</p>
+				<p><b>Databases and Cloud:</b> Git, PostgreSQL, MongoDB, Redis, RabbitMQ, nginx, Gunicorn, Spark, AWS EC2, S3</p>
+				<p><b>Languages:</b> English (C1), German (A2), Russian (C2), Armenian (B2)</p>
+			</div>
+		</section>
 	</TabItem>
 </Tabs>
 
-<section class="expirience">
-	<h1>Education</h1>
-	<div class="education">
-		<p><b>Data Science — Master of Science</b> <br> Technische Universität Wien, Austria, 2022 - now</p>
-		<p><b>Information Technologies and Telecommunications — Bachelor of Science</b> <br> Bauman Moscow State Technical University, 2014 - 2020</p>
-		<p><b>Deep Learning — Deep Learning School</b> <br> Moscow Institute of Physics and Technology, 2020 - 2021</p>
-		<p><b>Machine Learning — School of Data Analysis</b> <br> Higher School of Economics University and Yandex, 2020</p>
-	</div>
-</section>
-
-<section>
-	<h1>Work Experience</h1>
-	<div class="work-experience">
-		<p><b>Data Scientist, Project Rail4Future</b> <br> Technische Universität Wien, Feb 2024 - Jul 2024</p>
-		<ul>
-			<li>Analyzed rail irregularities, optimized LSTM models, and collaborated with Siemens and ÖBB.</li>
-			<li>Performed significance tests and improved model robustness and performance.</li>
-		</ul>
-		<p><b>CTO, Data Scientist</b> <br> susteam, Jan 2023 - present</p>
-		<ul>
-			<li>Developed architecture and Full Stack ESG system with OCR, Data Analysis, and Machine Learning.</li>
-			<li>Implemented RAG for LLMs using technologies like FastAPI, AWS, React.js, Bert, and LLama3.</li>
-		</ul>
-		<p><b>Machine Learning Analyst</b> <br> GLOSAV, Feb 2021 - Jul 2021</p>
-		<ul>
-			<li>Engineered algorithms for object detection and tracking in railway routes, contributing to a train autopilot system.</li>
-		</ul>
-		<p><b>Project Manager, 3D-Modeller, Software Engineer</b> <br> Arterra Miniature, Feb 2020 - Mar 2021</p>
-		<ul>
-			<li>Handled 3D visualization, prototyping, and project management from inception to delivery.</li>
-		</ul>
-	</div>
-</section>
-
-<section>
-	<h1>Projects and Awards</h1>
-	<div class="projects-awards">
-		<p><b>Tourism Technology Hackathon — 2nd Place Winner</b> <br> Nov 2023</p>
-		<ul>
-			<li>Developed a data pipeline and knowledge graph for tourism data, visualized as an interactive map with a chatbot interface.</li>
-		</ul>
-		<p><b>Coding Austria — Lip Reading App at Hackathon</b> <br> Jul 2023</p>
-		<ul>
-			<li>Utilized AV-HuBERT for lip reading, enabling communication in noisy environments for the Austrian Red Cross.</li>
-		</ul>
-	</div>
-</section>
-
-<section>
-	<h1>Skills</h1>
-	<div class="skills">
-		<p><b>Software Development:</b> Docker, Python, C++, R, Django, Flask, FastAPI, JavaScript, React, Svelte</p>
-		<p><b>Machine Learning:</b> scikit-learn, Pandas, NumPy, Tensorflow, PyTorch, OpenCV, YOLO, NLTK, SpaCy, Bert, LLama3, GPT</p>
-		<p><b>Databases and Cloud:</b> Git, PostgreSQL, MongoDB, Redis, RabbitMQ, nginx, Gunicorn, Spark, AWS EC2, S3</p>
-		<p><b>Languages:</b> English (C1), German (A2), Russian (C2), Armenian (B2)</p>
-	</div>
-</section>
+<div class="content">
+	<section class="expirience">
+		<h1>Education</h1>
+		<div class="education">
+			<p><b>Data Science — Master of Science</b> <br> Technische Universität Wien, 2022 - now</p>
+			<p><b>Information Technologies and Telecommunications — Bachelor of Science</b> <br> Bauman Moscow State Technical University, 2014 - 2020</p>
+			<p><b>Deep Learning — Deep Learning School</b> <br> Moscow Institute of Physics and Technology, 2020 - 2021</p>
+			<p><b>Machine Learning — School of Data Analysis</b> <br> Higher School of Economics University and Yandex, 2020</p>
+		</div>
+	</section>
+	
+	<section class="expirience">
+		<h1>Work Experience</h1>
+		<div class="work-experience">
+			<p><b>Data Scientist, Project Rail4Future</b> <br> Technische Universität Wien, Feb 2024 - Jul 2024</p>
+			<ul>
+				<li>Analyzed rail irregularities, optimized LSTM models, and collaborated with Siemens and ÖBB.</li>
+				<li>Performed significance tests and improved model robustness and performance.</li>
+			</ul>
+			<p><b>CTO, Data Scientist</b> <br> susteam, Jan 2023 - present</p>
+			<ul>
+				<li>Developed architecture and Full Stack ESG system with OCR, Data Analysis, and Machine Learning.</li>
+				<li>Implemented RAG for LLMs using technologies like FastAPI, AWS, React.js, Bert, and LLama3.</li>
+			</ul>
+			<p><b>Machine Learning Analyst</b> <br> GLOSAV, Feb 2021 - Jul 2021</p>
+			<ul>
+				<li>Engineered algorithms for object detection and tracking in railway routes, contributing to a train autopilot system.</li>
+			</ul>
+			<p><b>Project Manager, 3D-Modeller, Software Engineer</b> <br> Arterra Miniature, Feb 2020 - Mar 2021</p>
+			<ul>
+				<li>Handled 3D visualization, prototyping, and project management from inception to delivery.</li>
+			</ul>
+		</div>
+	</section>
+	
+	<section class="expirience">
+		<h1>Projects and Awards</h1>
+		<div class="projects-awards">
+			<p><b>Tourism Technology Hackathon — 2nd Place Winner</b> <br> Nov 2023</p>
+			<ul>
+				<li>Developed a data pipeline and knowledge graph for tourism data, visualized as an interactive map with a chatbot interface.</li>
+			</ul>
+			<p><b>Coding Austria — Lip Reading App at Hackathon</b> <br> Jul 2023</p>
+			<ul>
+				<li>Utilized AV-HuBERT for lip reading, enabling communication in noisy environments for the Austrian Red Cross.</li>
+			</ul>
+		</div>
+	</section>
+	
+	<section class="expirience">
+		<h1>Skills</h1>
+		<div class="skills">
+			<p><b>Software Development:</b> Docker, Python, C++, R, Django, Flask, FastAPI, JavaScript, React, Svelte</p>
+			<p><b>Machine Learning:</b> scikit-learn, Pandas, NumPy, Tensorflow, PyTorch, OpenCV, YOLO, NLTK, SpaCy, Bert, LLama3, GPT</p>
+			<p><b>Databases and Cloud:</b> Git, PostgreSQL, MongoDB, Redis, RabbitMQ, nginx, Gunicorn, Spark, AWS EC2, S3</p>
+			<p><b>Languages:</b> English (C1), German (A2), Russian (C2), Armenian (B2)</p>
+		</div>
+	</section>
+</div>
 
 <style>
 section {
@@ -201,9 +229,6 @@ p{
 	text-align: justify;
 }
 
-.orange {
-	color: var(--color-text-or);
-}
 h1 {
 	width: 100%;
 	color: var(--color-text-or);
@@ -282,24 +307,6 @@ b{
 	color: var(--color-text-or);
 }
 
-.contact {
-	/* margin-top: 20px; */
-	/* text-align: center; */
-	text-align: left;
-	font-size: 1.2rem;
-}
-
-.contact a {
-	color: var(--color-text-or);
-	text-decoration: none;
-	margin: 0 10px;
-}
-
-.contact a:hover {
-	text-decoration: underline;
-	color: white;
-}
-
 .blueShadow {
 box-shadow: 0px 14px 55px #4075a6;
 }
@@ -308,13 +315,20 @@ box-shadow: 0px 14px 55px #4075a6;
 box-shadow: 0px 14px 55px #7dc0ff;
 }
 
-
-
-.exprience {
+.expirience {
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+}
+
+.expirience > div {
+	padding-left: 3vh;
+}
+
+.expirience h1 {
+	color: var(--color-text-or);
+	font-size: 2rem;
+	font-weight: bold;
+	text-align: left;
 }
 
 .projects {
@@ -327,12 +341,20 @@ box-shadow: 0px 14px 55px #7dc0ff;
 		/* height: 100vh; */
 	}
 
+.content {
+	padding: 20px;
+	margin-top: 5vh;
+	display: flex;
+	flex-direction: column;
+}
+
 /* media mobile */
 @media (max-width: 768px) {
 	.landing {
 		flex-direction: column;
 		padding-top: 5vh;
 		background-position: center;
+		padding-bottom: 10vh;
 	}
 	.name-header {
 		width: 100vw;
@@ -347,9 +369,6 @@ box-shadow: 0px 14px 55px #7dc0ff;
 	}
 	.description > p {
 		font-size: 1rem;
-	}
-	.contact {
-		font-size: 0.8rem;
 	}
 }
 
