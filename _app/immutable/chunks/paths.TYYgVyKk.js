b50ebe599613038a1c98857f15d8fbc3e38e2b64
PropovedNik007 <PropovedNik007@users.noreply.github.com>
2024-10-23 19:15:01 +0000

    deploy: bbb74487c256233a601db4005acb8d981368ed7b

--- _app/immutable/chunks/paths.TYYgVyKk.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1hteoad)==null?void 0:s.base)??"/arterra";var a;const t=((a=globalThis.__sveltekit_1hteoad)==null?void 0:a.assets)??e;export{t as a,e as b};
--- _app/immutable/entry/start.CESw5z-V.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CRf5fe9o.js";export{t as start};
