687292d31f5bf90b1895558b2b20167763f1e730
PropovedNik007 <PropovedNik007@users.noreply.github.com>
2026-07-09 23:38:56 +0000

    deploy: 674757433f45b575c1ffff6761b4e95b7790cc40

--- _app/immutable/entry/start.CG_-BrLM.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BEaFlVjT.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.Cvgkp9_n.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DOXnnxuM.js";import{y as t}from"../chunks/H_uhrLxC.js";export{t as load_css,r as start};
